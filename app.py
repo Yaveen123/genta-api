188737c5646b46496ef27b00d1b7c6b58d9ce93b
Yaveen! <94953863+Yaveen123@users.noreply.github.com>
2025-05-27 11:25:26 +1000

    added editdb func

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from google.oauth2 import id_token
 from google.auth.transport import requests
 from functools import wraps
 import uuid
+from flask import redirect
+from datetime import date
 
 # Load the environment variables from the OS
 load_dotenv()
@@ -234,13 +236,243 @@ def getUserDataFromDB(user_google_id):
         "projects": assembled_projects_data
     }
 
+def editDatabase(user_db_id, projects_from_client_list, existing_projects_list_from_db):
+    processed_project_ids_from_client = set() #a set stores unique IDs and all project IDs should be unique
 
+    # Iterate through all projs to check if changes in project data
+    for project_client_data in projects_from_client_list: 
+        existing_project_in_db = None
+        current_project_id_in_db = None
+
+        # if theres any projects inside the client data
+        if project_client_data.get('id') != None:
+            
+            # find a project inside existing projects where id = id in project client data
+            existing_project_in_db = None
+            for project in existing_projects_list_from_db:
+                if project.id == project_client_data['id']:
+                    existing_project_in_db = project
+                    break
+
+            # this means that genta couldnt find a project where the id in db matches the cli id
+            # which should never happen (maybe a bad actor is trying to do something)
+            if existing_project_in_db == None:
+                return f"ERROR: Project with ID {project_client_data['id']} is not found in db for user"
+            
+            current_project_id_in_db = existing_project_in_db.id
+
+            updated = False
+            # If the project title has changed
+            if project_client_data.get('projectTitle') != existing_project_in_db.projectTitle:
+                existing_project_in_db.projectTitle = project_client_data['projectTitle']
+                updated = True
+            # if the duedate is within an obj or a str (could possibly be either bc idk how cli updates works atp)
+            # to compare dates genta has to conv str to date obj
+            # geeksforgeeks (2021) fromisoformat() function of datetime.date class in py https://www.geeksforgeeks.org/fromisoformat-function-of-datetime-date-class-in-python/ 
+            # w3schools (n.d.) python isinstance() https://www.w3schools.com/python/ref_func_isinstance.asp 
+            if isinstance(project_client_data.get('dueDate'), str):
+                client_due_date_obj = date.fromisoformat(project_client_data['dueDate'])
+            else:
+                client_due_date_obj = project_client_data.get('dueDate')
+            # if the client id has changed
+            if client_due_date_obj != existing_project_in_db.dueDate:
+                existing_project_in_db.dueDate = client_due_date_obj
+                updated = True
+            if updated:
+                db.session.add(existing_project_in_db) # Mark for update by putting it into staging area
+            
+            # add the UNIQUE processed id to the set!
+            processed_project_ids_from_client.add(current_project_id_in_db)
+
+        else:
+            # There isnt a project! so genta has to make a new one
+
+            # conv date obj to str in case
+            if isinstance(project_client_data.get('dueDate'), str):
+                client_due_date_obj = date.fromisoformat(project_client_data['dueDate']) 
+            else:
+                client_due_date_obj = project_client_data.get('dueDate')
+
+            # create a new project obj sqlalch
+            new_project = Project(
+                userId = user_db_id,
+                projectTitle = project_client_data['projectTitle'],
+                dueDate = client_due_date_obj
+            )
+            # add it to the db and get new id before commit for foreign keys
+            db.session.add(new_project) #stage
+            db.session.flush()
+            current_project_id_in_db = new_project.id
+            processed_project_ids_from_client.add(current_project_id_in_db) # add to list of processed ids
+
+        # handle changing events
+        # get the events
+        events_from_client = project_client_data.get('events', [])
+        # get from db and create a set for it
+        if existing_project_in_db: 
+            existing_events_in_db_for_project = existing_project_in_db.events
+        else:
+            existing_events_in_db_for_project = []
+        processed_event_ids_for_proj = set()
+
+        # if there are any events
+        for event_client_data in events_from_client:
+            existing_event_in_db = None
+            current_event_in_db = None
+            # if the event has an ID, make ure to check if the id a matches an event on the db
+            if event_client_data.get('id') != None:
+                
+                existing_event_in_db = None
+                for event in existing_events_in_db_for_project:
+                    if event.id == event_client_data['id']:
+                        existing_event_in_db = event
+                        break
+            
+                # checks to see if there was any match
+                if existing_event_in_db == None:
+                    # this should never happen because the database should always have something to edit 
+                    return f"ERROR: event with ID {event_client_data["id"]} is not found in table projects w id: {current_project_id_in_db}"
+                
+                current_event_id_in_db = existing_event_in_db.id
+                updated = False
+
+                # conv the date from str to date obj if its a str
+                if isinstance(event_client_data['dueDate'], str):
+                    client_event_due_date_obj = date.fromisoformat(event_client_data['dueDate'])
+                else:
+                    client_event_due_date_obj = event_client_data['dueDate']
+
+                # now, genta can go through the items and check if there has been a change
+                if event_client_data.get('title') != existing_event_in_db.title:
+                    existing_event_in_db.title = event_client_data['title']
+                    updated = True
+                if event_client_data.get('collapsed') != existing_event_in_db.collapsed:
+                    existing_event_in_db.collapsed = event_client_data['collapsed']
+                    updated = True
+                if client_event_due_date_obj != existing_event_in_db.dueDate:
+                    existing_event_in_db.dueDate = client_event_due_date_obj
+                    updated = True
+                if event_client_data.get('notes') != existing_event_in_db.notes:
+                    existing_event_in_db.notes = event_client_data['notes']
+                    updated = True
+                if event_client_data.get('todoShown') != existing_event_in_db.todoShown:
+                    existing_event_in_db.todoShown = event_client_data['todoShown']
+                    updated = True
+                if event_client_data.get('notesShown') != existing_event_in_db.notesShown:
+                    existing_event_in_db.notesShown = event_client_data['notesShown']
+                    updated = True
+                
+                if updated:
+                    # put the updates in session (stage) ready for db commit 
+                    db.session.add(existing_event_in_db)
+                
+                processed_event_ids_for_proj.add(current_event_id_in_db)
+            else:
+                # create new event
+                # conv to date obj 
+                if isinstance(event_client_data['dueDate'], str):
+                    client_event_due_date_obj = date.fromisoformat(event_client_data['dueDate'])
+                else:
+                    client_event_due_date_obj = event_client_data['dueDate']
+
+                # create the new event in the database
+                new_event = Event(
+                    projectId=current_project_id_in_db,
+                    title=event_client_data['title'],
+                    collapsed=event_client_data['collapsed'],
+                    dueDate=client_event_due_date_obj,
+                    notes=event_client_data.get('notes'), # turns out i have to use .get() because the notes section can be null (stupid)
+                    todoShown=event_client_data['todoShown'],
+                    notesShown=event_client_data['notesShown']
+                )
+                # add changes to stage, get new id
+                db.session.add(new_event)
+                db.session.flush() 
+                # get id and log 
+                current_event_id_in_db = new_event.id
+                processed_event_ids_for_proj.add(current_event_id_in_db)
+
+            # time for the todos 
+            # get todos from client otherwise use empty list
+            todos_from_client = event_client_data.get('todo', [])
+            # get the todos if they exist
+            if existing_event_in_db:
+                existing_todos_in_db_for_event = existing_event_in_db.todos
+            else:
+                existing_todos_in_db_for_event = []
+
+            # set the id storage
+            processed_todos_for_proj = set()
+
+            # for every todo from client
+            for todo_client_data in todos_from_client:
+                existing_todo_in_db = None
+
+                #  if there are actually todos
+                if todo_client_data.get('id') != None:
+                    # get todos that exist
+                    existing_todo_in_db = None
+                    for todo in existing_todos_in_db_for_event:
+                        if todo.id == todo_client_data['id']:
+                            existing_todo_in_db = todo
+                            break
+                    # otherwise raise an err (should never happen)
+                    if existing_todo_in_db == None:
+                        return f"ERROR: todo with id {todo_client_data["id"]} cannot be found"
+                    
+                    # now we can check each part of the todo item thing tos ee if its changed
+                    current_todo_id_in_db = existing_todo_in_db.id
+                    updated = False
+                    if todo_client_data.get('checked') != existing_todo_in_db.checked:
+                        existing_todo_in_db.checked = todo_client_data['checked']
+                        updated = True
+                    if todo_client_data.get('content') != existing_todo_in_db.content:
+                        existing_todo_in_db.content = todo_client_data['content']
+                        updated = True
+                    # if changes are present stage them for committing
+                    if updated:
+                        db.session.add(existing_todo_in_db)
+                    
+                    processed_todos_for_proj.add(current_todo_id_in_db)
+                else:
+                    # create a new todo cuz it aint exist bru :(
+                    # note for future readers if this sucks im way too tired this logic hurts my brain
+                    new_todo = Todo(
+                        eventId=current_event_id_in_db,
+                        content=todo_client_data.get('content'),
+                        checked=todo_client_data['checked']
+                    )
+                    db.session.add(new_todo)
+            
+            # del todos for event
+            for existing_todo_item in existing_todos_in_db_for_event:
+                if existing_todo_item.id not in processed_todos_for_proj:
+                    db.session.delete(existing_todo_item)
+        
+        # del events for proj 
+        for existing_event_item in existing_events_in_db_for_project:
+            if existing_event_item.id not in processed_event_ids_for_proj:
+                db.session.delete(existing_event_item)
+
+    # Delete projects not in client data for user!
+    for existing_project_item in existing_projects_list_from_db:
+        if existing_project_item.id not in processed_project_ids_from_client:
+            db.session.delete(existing_project_item)
+
+    # try to commit all staged changes
+    try:
+        db.session.commit()
+        return "SUCCESS"
+    except Exception as err:
+        db.session.rollback()
+        print(f"problem w database {err}")
+        return f"Database update failed: {err}"
 
 
 
 @app.route('/')
 def home():
-    return "Welcome to Genta API"
+    return "Welcome to GentaAPI"
 
 if __name__ == '__main__':
     with app.app_context():
